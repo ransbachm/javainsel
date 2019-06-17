e54a502332dae71a9bc1189cff201fc2d64f2731
ransbachm <ransbachm@gmail.com>
2019-06-17 17:40:16 +0200

    Create ImageNeigbourDownsampler.java
    Scales multiple images down, somehow.

---
+++ src/main/java/com/insel/noChapter/ImageNeigbourDownsampler.java
@@ -0,0 +1,47 @@
+package com.insel.noChapter;
+
+import java.awt.List;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+public class ImageNeigbourDownsampler {
+	public static void main(String [] args) throws IOException {
+		
+		
+		File inFile = new File("C:\\MyF\\ImageIO\\DSInput.png");
+		BufferedImage imageIn = ImageIO.read(inFile);
+		int maxScale = 4;
+		int minScale = 1;
+		
+		for(int scale=minScale; scale!=maxScale; scale++) {
+			File outFile = new File("C:\\MyF\\ImageIO\\DSOutput" + scale + ".png");
+			
+			int width = imageIn.getWidth();
+			int height = imageIn.getHeight();
+			BufferedImage imageOut = new BufferedImage((int) Math.ceil((double) width/scale),
+					(int) Math.ceil((double) height/scale), BufferedImage.TYPE_4BYTE_ABGR);
+					
+			//System.out.println(width + " " + height);
+			//System.out.println(imageOut.getWidth() + " " + imageOut.getHeight());
+			
+			
+			
+			for(int y=0; y<height; y+=scale) {
+				for(int x=0; x<width; x+=scale) {
+					//System.out.println(x/scale+" "+ y);
+					imageOut.setRGB(x/scale, y/scale, imageIn.getRGB(x, y) );
+					
+				}
+			}
+			ImageIO.write(imageOut, "png", outFile);
+		}
+		
+		System.out.println("Done!");
+		
+	}
+
+}
