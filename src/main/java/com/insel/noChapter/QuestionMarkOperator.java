54af8881523dc99d01c10e070855b8a9d8bd604e
ransbachm <ransbachm@gmail.com>
2019-07-06 23:39:28 +0200

    Created QuestionMarkOperator
    Example how to use the ? operator.

---
+++ src/main/java/com/insel/noChapter/QuestionMarkOperator.java
@@ -0,0 +1,16 @@
+package com.insel.noChapter;
+
+public class QuestionMarkOperator {
+	
+	public static void main (String [] args) {
+		int a = 2;
+		int b = 5;
+		String c;
+		
+		c = ((a>b) ? "A ist größer!" : "B ist größer!");
+		
+		System.out.println(c);
+		
+	}
+
+}
