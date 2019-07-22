a9aa3fa147cde5109fb3b2e3cbc4670bb630d662
ransbachm <ransbachm@gmail.com>
2019-07-22 12:37:11 +0200

    Create Callable and Phone classes
    Example classes for forcing enum subclasses to implement interfaces.

---
+++ src/main/java/com/insel/chapter10/Callable.java
@@ -0,0 +1,5 @@
+package com.insel.chapter10;
+
+public interface Callable {
+	public void recieve();
+}
---
+++ src/main/java/com/insel/chapter10/Phone.java
@@ -0,0 +1,17 @@
+package com.insel.chapter10;
+
+public enum Phone implements Callable {
+	
+	BPhoneY() {
+		@Override public void recieve () {
+			//Do stuff
+		}
+	},
+	
+	TwoMinus42() {
+		@Override public void recieve() {
+			//Do stuff
+		}
+	};
+
+}
