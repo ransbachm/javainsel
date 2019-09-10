6c879ef981a3675d384f30347e47db8915214f1b
ransbachm <ransbachm@gmail.com>
2019-09-10 22:46:23 +0200

    Update WaitThread.java
    Now uses RunnableProvider

--- src/main/java/com/insel/chapter16/WaitThread.java
+++ src/main/java/com/insel/chapter16/WaitThread.java
@@ -1,12 +1,11 @@
 package com.insel.chapter16;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 public class WaitThread {
 	public static void main(String [] args) throws InterruptedException {
-		Thread a = new Thread(getWaitRunnable());
-		Thread b = new Thread(getWaitRunnable());
-		Thread c = new Thread(getWaitRunnable());
+		
+		Thread a = new Thread(RunnableProvider.getWaitRunnable());
+		Thread b = new Thread(RunnableProvider.getWaitRunnable());
+		Thread c = new Thread(RunnableProvider.getWaitRunnable());
 		
 		a.start();
 		b.start();
@@ -18,23 +17,6 @@ public class WaitThread {
 		
 		System.out.println("Done!");
 		
-	}
-	
-	
-	
-	
-	public static Runnable getWaitRunnable() {
-		return () -> {
-			// 1000 + 1 to include 1000, compiler shold optimize that 'away'
-			int rand = ThreadLocalRandom.current().nextInt(1, 1000+1);
-			System.out.printf("Waiting: %d ms %s%n", rand, Thread.currentThread().getName());
-			try {
-				Thread.sleep(rand);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			System.out.printf("Done waiting: %s%n", Thread.currentThread().getName());
-		};
 	}
 
 }
